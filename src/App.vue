96ef1b1e83499906909c019ec7af44fafdf93ecb
Gabriel Cacayan <gabrielcacayan@gmail.com>
2023-04-30 16:14:17 +0800

    Added files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <v-app>
+    <v-main class="d-flex justify-center align-center bg-grey-lighten-4">
+      <v-container class="d-flex justify-center align-center">
+        <v-card
+          title="Gabriel Cacayan"
+          subtitle="Web Developer"
+          text="As a web developer, my focus is on creating engaging and
+                user-friendly websites and applications. I have experience
+                working with a range of technologies, but I particularly enjoy
+                using Laravel and Vue, which I find to be powerful and effective
+                tools for building high-quality web projects."
+          variant="outlined"
+          max-width="344"
+          class="bg-white"
+        ></v-card>
+      </v-container>
+    </v-main>
+
     <!-- <v-switch
       v-model="theme"
       label="Dark Mode"
@@ -8,7 +25,7 @@
       hide-details
     ></v-switch>
     {{ theme }} -->
-    <TheHeader :drawer="drawer" />
+    <!-- <TheHeader :drawer="drawer" />
     <v-main>
       <template v-if="tab == 'Home'">
         <HomeDetail />
@@ -23,24 +40,24 @@
         {{ new Date().getFullYear() }} —
         <strong class="ml-2">Gabriel Cacayan</strong></v-footer
       >
-    </v-main>
+    </v-main> -->
   </v-app>
 </template>
 
 <script>
-import ExperienceDetail from "./components/ExperienceDetail.vue";
-import HomeDetail from "./components/HomeDetail.vue";
-import ProjectDetail from "./components/ProjectDetail.vue";
-import TheHeader from "./components/TheHeader.vue";
+// import ExperienceDetail from "./components/ExperienceDetail.vue";
+// import HomeDetail from "./components/HomeDetail.vue";
+// import ProjectDetail from "./components/ProjectDetail.vue";
+// import TheHeader from "./components/TheHeader.vue";
 
 export default {
   name: "App",
 
   components: {
-    ExperienceDetail,
-    TheHeader,
-    HomeDetail,
-    ProjectDetail,
+    // ExperienceDetail,
+    // TheHeader,
+    // HomeDetail,
+    // ProjectDetail,
   },
 
   data() {
--- src/components/HomeDetail.vue
+++ src/components/HomeDetail.vue
@@ -1,5 +1,21 @@
 <template>
   <v-container>
+    <v-card class="mx-auto" max-width="344" variant="outlined">
+      <v-card-item>
+        <div>
+          <div class="text-overline mb-1">OVERLINE</div>
+          <div class="text-h6 mb-1">Headline</div>
+          <div class="text-caption">
+            Greyhound divisely hello coldly fonwderfully
+          </div>
+        </div>
+      </v-card-item>
+
+      <v-card-actions>
+        <v-btn variant="outlined"> Button </v-btn>
+      </v-card-actions>
+    </v-card>
+
     <v-row align="center" justify="center">
       <v-col cols="12" md="6">
         <v-img
